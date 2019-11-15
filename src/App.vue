7c4bc864e5163bfc6320386b8c24990ef3585589
MarekGracz <mgraczykowski@gmail.com>
2019-11-15 11:36:36 +0100

    basic playout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Player :source="source" v-if="source"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Player from "./components/Player.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Player
+  },
+  data: function() {
+    return {
+      source: [
+        {
+          type: "application/x-mpegurl",
+          src:
+            "http://cdn.theoplayer.com/video/big_buck_bunny/big_buck_bunny_metadata.m3u8"
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Player.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="theoplayer-container video-js theoplayer-skin" ref="theoplayer"></div>
+</template>
+
+<script>
+export default {
+  props: ["source"],
+  mounted: function() {
+    this.playerInit();
+  },
+  methods: {
+    playerInit() {
+      const player = new window.THEOplayer.Player(this.$refs.theoplayer, {
+        fluid: true,
+        libraryLocation: "./libs/THEOplayer"
+      });
+      player.source = {
+        sources: this.source
+      };
+    }
+  }
+};
+</script>
